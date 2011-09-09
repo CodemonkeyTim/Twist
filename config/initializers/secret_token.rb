6a43baeb3d06c7a6b57668b3ead1c3f997275797
Teemu Vainio <tim@silvertreksystems.com>
2011-09-09 16:40:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twist::Application.config.secret_token = 'be83bba0c489efa98060ed433907028ca93a97d9cbf3f7b669f9d5b880bb1d61a0ab07ea13d0850f7720d8aed0bbbb5faf218e27f5be226dab1a42a34fdd092f'
